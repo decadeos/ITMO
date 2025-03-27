ade23a4bd22742a2830772b0784321c589752c9b
decadeos <ohrimenkoed@gmail.com>
2025-03-27 15:50:29 +0300

    done all noise (вроде)

--- 2_course/texViz/lab3/code/noises.py
+++ 2_course/texViz/lab3/code/noises.py
@@ -19,9 +19,37 @@ def imNoise(image, amount=0.5):
 
 
 def addNoise(image, noise):
-    noisy_image = image + noise
-    return np.clip(noisy_image, 0, 255).astype(np.uint8)
+    noisyImage = image + noise
+    return np.clip(noisyImage, 0, 255).astype(np.uint8)
 
 def mulNoise(image, noise):
-    noisy_image = image * noise
-    return np.clip(noisy_image, 0, 255).astype(np.uint8)
+    noisyImage = image * noise
+    return np.clip(noisyImage, 0, 255).astype(np.uint8)
+
+def gayNoise(image, mean, var): # (хихихи)
+    rng = np.random.default_rng()
+    gauss = (rng.normal(mean, var**0.5, image.shape)).reshape(image.shape)
+    if image.dtype == np.uint8:
+        noisyImage = (image.astype(np.float32) + gauss*255).clip(0, 255).astype(np.uint8)
+    else :
+        noisyImage = (image+gauss).astype(np.float32)
+    return noisyImage
+
+def kvantNoise(I):
+    rng = np.random.default_rng()
+
+    if I.dtype == np.uint8:
+        I_f = I . astype ( np . float32 ) / 255
+        vals = len ( np . unique ( I_f ))
+        vals = 2 ** np . ceil ( np . log2 ( vals ))
+        I_out = (255 * \
+        ( rng . poisson ( I_f * vals ) / \
+        float ( vals )). clip (0 , 1)). \
+        astype ( np . uint8 )
+    else :
+        vals = len ( np . unique ( I ))
+        vals = 2 ** np . ceil ( np . log2 ( vals ))
+        I_out = \
+        rng . poisson ( I * vals ) / float ( vals )
+
+    return I_out
--- 2_course/texViz/lab3/code/py.py
+++ 2_course/texViz/lab3/code/py.py
@@ -3,12 +3,18 @@ from noises import *
 
 image = cv2.imread("../images/flowers.jpg")
 
-imImage = imNoise(image, 0.5)
+imImage = imNoise(image, 0.1)
 plot(imImage, "impulsny", "../noise/impuls.png")
 
-addImage = addNoise(image, 1)
+addImage = addNoise(image, 100)
 plot(addImage, "additivny", "../noise/additive.png")
 
 mulImage = mulNoise(image, 5)
 plot(mulImage, "multiplicativny", "../noise/multiplicativny.png")
+
+gayImage = gayNoise(image, 1, 12)
+plot(gayImage, "gaussian", "../noise/gaussian.png")
+
+kvantImage = kvantNoise(image)
+plot(kvantImage, "kvant", "../noise/kvant.png")
 
