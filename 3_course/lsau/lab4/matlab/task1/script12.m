48b41aa0eb223b6e83f6d9103db8d0258223090d
eva <ohrimenkoed@gmail.com>
2025-10-21 16:48:31 +0300

    done task 1

--- 3_course/lsau/lab4/matlab/task1/script12.m
+++ 3_course/lsau/lab4/matlab/task1/script12.m
@@ -1,5 +1,28 @@
 a2 = 1; a1 = 1; a0 = -2;
 k1 = -1.5; k0 = -15;
 
-model_name = gcs; 
-print(['-s' model_name], '-dpng', '-r300', '../../images/task1/model.png');
+out = sim('model1');
+% print(['-s' gcs], '-dpng', '-r300', '../../images/task1/model.png');
+
+t = out.y.time;
+y = out.y.signals.values;
+
+figure('Color', 'white', 'Position', [100, 100, 900, 400]);
+
+plot(t,y, ...
+    'LineWidth', 2.5, ...
+    'Color', [0.0, 0.0, 0.7]);
+
+ylim([-0.2, 0.1]); xlim([0, 7.05]); 
+xlabel('Time'); ylabel('Amplitude');
+legend('y_{zamk}(t)', 'Location', 'northeast', 'FontSize', 15)
+
+ax = gca; ax.XTick = 0:1:10; 
+ax.LineWidth = 1.5; ax.FontSize = 12;
+ax.XColor = [0.3, 0.3, 0.3]; ax.YColor = [0.3, 0.3, 0.3];
+ax.GridColor = [0.9, 0.9, 0.9]; ax.GridAlpha = 0.4;
+ax.MinorGridColor = [0.95, 0.95, 0.95]; ax.MinorGridAlpha = 0.2;
+grid on; grid minor; box on; grid on;
+
+set(findall(gcf, '-property', 'FontName'), 'FontName', 'DejaVu Math TeX Gyre');
+exportgraphics(gcf, '../../images/task1/y.png', 'Resolution', 300);
--- 3_course/lsau/lab4/matlab/task2/script2.m
+++ 3_course/lsau/lab4/matlab/task2/script2.m
@@ -1,28 +1,29 @@
 a2 = 1; a1 = 1; a0 = -2;
-y0 = 0; dy0 = -1; 
-odefun = @(t, x) [x(2);
-                  ( -a1*x(2) - a0*x(1) )/a2 ];
-tspan = [0 10];
-x0 = [y0; dy0];
-[t, x] = ode45(odefun, tspan, x0);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+k1 = -1.5; k0 = -15;
+T = 0.5;
+
+out = sim('model2');
+print(['-s' gcs], '-dpng', '-r300', '../../images/task2/model.png');
+
+t = out.y.time;
+y = out.y.signals.values;
+
 figure('Color', 'white', 'Position', [100, 100, 900, 400]);
 
-plot(t, x(:,1), ...
+plot(t,y, ...
     'LineWidth', 2.5, ...
     'Color', [0.0, 0.0, 0.7]);
 
-ylim([-355, 5]); xlim([0, 7.05]); 
+ylim([-0.2, 0.1]); xlim([0, 7.05]); 
 xlabel('Time'); ylabel('Amplitude');
-legend('y_{paz}(t)', 'Location', 'northeast', 'FontSize', 15)
+legend('y_{zamk}(t)', 'Location', 'northeast', 'FontSize', 15)
 
 ax = gca; ax.XTick = 0:1:10; 
 ax.LineWidth = 1.5; ax.FontSize = 12;
 ax.XColor = [0.3, 0.3, 0.3]; ax.YColor = [0.3, 0.3, 0.3];
 ax.GridColor = [0.9, 0.9, 0.9]; ax.GridAlpha = 0.4;
 ax.MinorGridColor = [0.95, 0.95, 0.95]; ax.MinorGridAlpha = 0.2;
-
 grid on; grid minor; box on; grid on;
 
 set(findall(gcf, '-property', 'FontName'), 'FontName', 'DejaVu Math TeX Gyre');
-exportgraphics(gcf, '../../images/task1/y_raz.png', 'Resolution', 300);
+exportgraphics(gcf, '../../images/task2/y.png', 'Resolution', 300);
